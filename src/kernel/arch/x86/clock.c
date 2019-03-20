f9dadd9ab791bfa8af68143dbdf9f252d6c93636
Hugh Natt <hughnatt@protonmail.com>
2019-03-20 09:06:55 +0100

    Implemented resetframe for resetting accessed bit

--- src/kernel/arch/x86/clock.c
+++ src/kernel/arch/x86/clock.c
@@ -28,24 +28,34 @@ PUBLIC unsigned ticks = 0;
 /* Time at system startup. */
 PUBLIC unsigned startup_time = 0;
 
+/* Time until reset R bits */
+PUBLIC unsigned reset = 50;
+
 /*
  * Handles a timer interrupt.
  */
 PRIVATE void do_clock()
 {
 	ticks++;
-	
+
 	if (KERNEL_RUNNING(curr_proc))
 	{
 		curr_proc->ktime++;
 		return;
 	}
-	
+
 	curr_proc->utime++;
-		
+
 	/* Give up processor time. */
 	if (--curr_proc->counter == 0)
 		yield();
+
+	/* Resetting the frames */
+	if (--reset == 0)
+	{
+		resetframes();
+		reset = 50;
+	}
 }
 
 /*
@@ -54,16 +64,16 @@ PRIVATE void do_clock()
 PUBLIC void clock_init(unsigned freq)
 {
 	uint16_t freq_divisor;
-	
+
 	kprintf("dev: initializing clock device driver");
-	
+
 	set_hwint(INT_CLOCK, &do_clock);
-	
-	freq_divisor = PIT_FREQUENCY/freq;
-	
+
+	freq_divisor = PIT_FREQUENCY / freq;
+
 	/* Send control byte: adjust frequency divisor. */
 	outputb(PIT_CTRL, 0x36);
-	
+
 	/* Send data byte: divisor_low and divisor_high. */
 	outputb(PIT_DATA, (byte_t)(freq_divisor & 0xff));
 	outputb(PIT_DATA, (byte_t)((freq_divisor >> 8)));
